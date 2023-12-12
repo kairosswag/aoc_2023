37136b6cafdab83403d47a38856943bfbe350e75
Kai Roßwag <kai.rosswag@horus.biz>
2023-12-12 21:52:34 +0100

    Solve Day 11 part 2

--- src/day11.rs
+++ src/day11.rs
@@ -5,29 +5,39 @@ use std::str::Lines;
 pub fn run() {
     let file = fs::read_to_string("input/day11").expect("Could not open file.");
 
-    let universe = parse_universe(file.lines());
-    let res_1: usize = universe
-        .iter()
-        .map(|galaxy_1| {
-            universe
-                .iter()
-                .map(|galaxy_2| calc_distance(galaxy_1, galaxy_2))
-                .sum::<usize>()
-        })
-        .sum();
-    println!("Day 11 Solution Part 1: {}", res_1 / 2);
-    println!("Day 11 Solution Part 2: {}", 5);
+    let universe = parse_universe(file.lines(), 1);
+    let res_1 = solve(&universe);
+
+    let universe = parse_universe(file.lines(), 1000000);
+    let res_2 = solve(&universe);
+
+    println!("Day 11 Solution Part 1: {}", res_1);
+    println!("Day 11 Solution Part 2: {}", res_2);
+}
+
+fn solve(universe: &HashSet<(usize, usize)>) -> usize {
+    let mut total = 0;
+    let mut visited = HashSet::new();
+    for galaxy_a in universe {
+        for galaxy_b in universe {
+            if !visited.contains(&(galaxy_b, galaxy_a)) {
+                visited.insert((galaxy_a, galaxy_b));
+                total += calc_distance(galaxy_a, galaxy_b);
+            }
+        }
+    }
+    total
 }
 
 fn calc_distance((g1_x, g1_y): &(usize, usize), (g2_x, g2_y): &(usize, usize)) -> usize {
     if g1_x == g2_x && g1_y == g2_y {
         0
     } else {
-        g1_x.max(g2_x) - g1_x.min(g2_x) + g1_y.max(g2_y) - g1_y.min(g2_y)
+        (g1_x.max(g2_x) - g1_x.min(g2_x)) + (g1_y.max(g2_y) - g1_y.min(g2_y))
     }
 }
 
-fn parse_universe(lines: Lines) -> HashSet<(usize, usize)> {
+fn parse_universe(lines: Lines, expansion_factor: usize) -> HashSet<(usize, usize)> {
     let mut empty_x = vec![true; 150];
     let mut empty_y = vec![true; 150];
     let mut universe = HashSet::new();
@@ -47,7 +57,7 @@ fn parse_universe(lines: Lines) -> HashSet<(usize, usize)> {
     let mut real_universe = HashSet::new();
     for (galaxy_xpos, galaxy_ypos) in universe {
         let count_spaces = |spaaaace: &[bool], pos: usize| -> usize {
-            spaaaace[0..pos].iter().filter(|val| **val).count()
+            spaaaace[0..pos].iter().filter(|val| **val).count() * (expansion_factor - 1).max(1)
         };
 
         let real_xpos = galaxy_xpos + count_spaces(&empty_x, galaxy_xpos);
@@ -72,16 +82,16 @@ fn test() {
 .......#..
 #...#....."#;
 
-    let universe = parse_universe(test_input.lines());
-    let res_1: usize = universe
-        .iter()
-        .map(|galaxy_1| {
-            universe
-                .iter()
-                .map(|galaxy_2| calc_distance(galaxy_1, galaxy_2))
-                .sum::<usize>()
-        })
-        .sum();
+    let universe = parse_universe(test_input.lines(), 1);
+    let test_0 = solve(&universe);
 
-    assert_eq!(374, res_1 / 2);
+    let universe = parse_universe(test_input.lines(), 10);
+    let test_1 = solve(&universe);
+
+    let universe = parse_universe(test_input.lines(), 100);
+    let test_2 = solve(&universe);
+
+    assert_eq!(374, test_0);
+    assert_eq!(1030, test_1);
+    assert_eq!(8410, test_2);
 }
